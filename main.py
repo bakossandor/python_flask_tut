a9a0074dc819f062239661a16e346ed253c4daf3
Sándor Bakos <bakos.webd@gmail.com>
2019-01-26 15:55:30 +0100

    db configured schemas added

--- main.py
+++ main.py
@@ -1,9 +1,89 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 from config import DevConfig
+from flask_migrate import Migrate
+import datetime
 
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+class User(db.Model):
+    __tablename__ = 'user_table_name'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref='user',
+        lazy='dynamic'
+    )
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+
+tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user_table_name.id'))
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=True, unique=True)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
 
 
 @app.route('/')
